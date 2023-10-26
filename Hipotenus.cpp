affe99d26b2ad5ce2573444a88ec4dac858c8f06
goktugycr <116160230+goktugycr@users.noreply.github.com>
2023-10-26 21:34:07 +0300

    Add files via upload

---
+++ Hipotenus.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include<cmath>
+float sayi1;
+float sayi2;
+float karelertoplami;
+float hipotenus;
+void sayilariAl();
+int hipotenusHesapla();
+void ekranaBas();
+
+int main()
+{
+    sayilariAl();
+    hipotenusHesapla();
+    ekranaBas();
+}
+
+void sayilariAl() {
+    std::cout << "Dik Ucgenin Dik Kenarlarının Uzunluklarini Giriniz." << std::endl;
+    std::cin >> sayi1;
+    std::cin >> sayi2;
+}
+
+int hipotenusHesapla() {
+    karelertoplami = pow(sayi1, 2) + pow(sayi2, 2);
+    hipotenus = sqrt(karelertoplami);
+    return hipotenus;
+}
+
+void ekranaBas() {
+    std::cout << "hipotenus: " << hipotenus << std::endl;
+}
+
+
