e59de4bbc491ad5d8b632a71067145712e089932
goktugycr <116160230+goktugycr@users.noreply.github.com>
2023-10-28 00:15:21 +0300

    Add files via upload

---
+++ AsalBulma.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+int sayiAl();
+bool asalKontrol();
+void ekranaBas();
+int sayi;
+
+int main()
+{
+    sayiAl();
+    asalKontrol();
+    ekranaBas();
+}
+
+int sayiAl()
+{
+    std::cout << "Bir sayi giriniz" << std::endl;
+    std::cin >> sayi;
+    return sayi;
+}
+
+bool asalKontrol()
+{
+    if (sayi <= 1) return false;
+
+    for (int i = 2; i <= sayi / 2; i++)
+    {
+        if (sayi % i == 0)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+void ekranaBas()
+{
+    if (asalKontrol() == false)
+    {
+        std::cout << "Sayi asal degildir";
+    }
+    else
+        std::cout << "Sayi asaldir";
+
+}
+
---
+++ Faktoriyel.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+int sayiAl();
+int faktoriyelBul();
+void ekranaBas();
+int sayi;
+
+int main()
+{
+    sayiAl();
+    faktoriyelBul();
+    ekranaBas();
+}
+
+int sayiAl()
+{
+    std::cout << "Bir sayi giriniz" << std::endl;
+    std::cin >> sayi;
+    return sayi;
+}
+
+int faktoriyelBul()
+{
+    int fak = 1;
+    for (int i = sayi; i >= 1; i--)
+    {        
+        fak *= i;
+    }
+    return fak;
+}
+
+void ekranaBas()
+{
+    std::cout << "Sonuc: " << sayi << std::endl;
+}
+
