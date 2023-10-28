1f1bd7bdc33143b71393838fba53e3368df0ac3a
goktugycr <116160230+goktugycr@users.noreply.github.com>
2023-10-29 00:10:41 +0300

    Add files via upload

---
+++ HesapMakinesi.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    double sayi1, sayi2;
+    char islem;
+
+    cout << "Iki sayi giriniz: ";
+    cin >> sayi1 >> sayi2;
+    cout << "Bir islem seciniz (+, *, -, /): ";
+    cin >> islem;
+
+    switch (islem) {
+    case '+':
+        cout << "Sonuc: " << sayi1 + sayi2 << endl;
+        break;
+    case '*':
+        cout << "Sonuc: " << sayi1 * sayi2 << endl;
+        break;
+    case '-':
+        cout << "Sonuc: " << sayi1 - sayi2 << endl;
+        break;
+
+    case '/':
+        if (sayi2 != 0)
+            cout << "Sonuc: " << sayi1 / sayi2 << endl;
+        else
+            cout << "Bir sayiyi 0'a bolemezsiniz!" << endl;
+        break;
+    default:
+        cout << "Gecersiz islem!" << endl;
+    }
+
+    return 0;
+}
+
+
---
+++ NotHesaplama.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+int not1;
+int not2;
+int not3;
+float ort;
+bool kontrol = not1 > 100 || not2 > 100 || not3 > 100;
+void notAl();
+float ortHesapla();
+void ekranaBas();
+int main()
+{
+    notAl();
+    ort = ortHesapla();
+    ekranaBas();
+}
+
+void notAl()
+{
+    std::cout << "Uc sinav notunuzu sirasiyla girin" << std::endl;
+    std::cin >> not1 >> not2 >> not3;
+
+}
+
+float ortHesapla() {
+    bool kontrol = not1 > 100 || not2 > 100 || not3 > 100;
+    if (kontrol) {
+        std::cout << "Yanlis giris yaptiniz" << std::endl;
+        return 0;
+    }
+    else {
+        return not1 * 0.2 + not2 * 0.7 + not3 * 0.3;
+    }
+}
+
+void ekranaBas() {
+    if (ort != 0) {
+        std::cout << "Ortalamaniz: " << ort << std::endl;
+    }
+}
