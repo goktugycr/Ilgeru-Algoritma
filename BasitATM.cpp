d6900e0846d049ed1f7753dd247397443f38104b
goktugycr <116160230+goktugycr@users.noreply.github.com>
2023-10-29 23:02:53 +0300

    Add files via upload

---
+++ BasitATM.cpp
@@ -0,0 +1,55 @@
+﻿#include <iostream>
+
+float bakiye = 1000;
+
+void paraCek() {
+    float miktar;
+    std::cout << "Cekmek istediginiz miktari girin: ";
+    std::cin >> miktar;
+    if (miktar <= bakiye) {
+        bakiye -= miktar;
+        std::cout << "Kalan bakiye: " << bakiye << std::endl;
+    }
+    else {
+        std::cout << "Yetersiz bakiye." << std::endl;
+    }
+}
+
+void paraYatir() {
+    float miktar;
+    std::cout << "Yatirmak istediginiz miktari girin: ";
+    std::cin >> miktar;
+    bakiye += miktar;
+    std::cout << "Yeni bakiye: " << bakiye << std::endl;
+}
+
+void bakiyeSorgula() {
+    std::cout << "Mevcut bakiyeniz: " << bakiye << std::endl;
+}
+
+int main() {
+    int secim;
+    do {
+        std::cout << "1. Bakiye Sorgula\n2. Para Cek\n3. Para Yatir\n0. Cikis\nSeciminiz: ";
+        std::cin >> secim;
+
+        switch (secim) {
+        case 1:
+            bakiyeSorgula();
+            break;
+        case 2:
+            paraCek();
+            break;
+        case 3:
+            paraYatir();
+            break;
+        case 0:
+            std::cout << "Cikis yapiliyor..." << std::endl;
+            break;
+        default:
+            std::cout << "Gecersiz secim!" << std::endl;
+        }
+    } while (secim != 0);
+
+    return 0;
+}
---
+++ SayiTahmin.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+#include<windows.h>   
+int sayiOlustur();
+int tahminEt();
+void kontrolEt();
+int sayi;
+int tahmin;
+bool sayiMi = true;;
+
+int main()
+{
+    srand(time(nullptr));
+    sayiOlustur();
+    tahminEt();
+    kontrolEt();
+}
+
+int sayiOlustur()
+{
+    std::cout << "Rastgele sayi olusturuluyor..." << std::endl;
+    std::cout << "------" << std::endl;
+    Sleep(1000);
+    std::cout << "---------" << std::endl;
+    Sleep(1000);
+    std::cout << "------------" << std::endl;
+    sayi = rand() % 20;
+    return sayi;
+}
+
+int tahminEt()
+{
+    std::cout << "Sayiyi tahmin edin" << std::endl;
+    std::cin >> tahmin;
+    return tahmin;
+}
+
+void kontrolEt()
+{
+    if (tahmin == sayi)
+    {
+       std::cout << "Tebrikler tahmininiz dogru" << std::endl;
+    }
+    else if (tahmin != sayi)
+    {
+        std::cout << "Uzgunum tahmininiz yanlis sayi " << sayi << " idi" << std::endl;
+    }       
+}
+
+ 
+
