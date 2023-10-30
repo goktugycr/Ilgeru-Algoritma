b4af6c001dada28df44bcb135e2844abd21206f1
goktugycr <116160230+goktugycr@users.noreply.github.com>
2023-10-31 01:09:03 +0300

    Add files via upload

---
+++ BedenKitleIndeksi.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+float kilo;
+float boy;
+void bedenKitleIndeksiHesapla();
+
+int main()
+{
+    bedenKitleIndeksiHesapla();
+}
+
+void bedenKitleIndeksiHesapla()
+{
+    std::cout << "Lutfen kilonuzu girin (kg)" << std::endl;
+    std::cin >> kilo;
+    std::cout << "Lutfen boyunuzu girin (m)" << std::endl;
+    std::cin >> boy;
+    float BKI = kilo / pow(boy, 2);
+    if (BKI < 18.5)
+    {
+        std::cout << "Zayif" << std::endl;
+    }
+    else if (BKI >= 18.5 && BKI < 24.9)
+    {
+        std::cout << "Normal" << std::endl;
+    }
+    else if (BKI >= 24.9 && BKI < 29.9)
+    {
+        std::cout << "Kilolu" << std::endl;
+    }
+    else if (BKI >= 30)
+    {
+        std::cout << "Obez" << std::endl;
+    }
+}
+
+
---
+++ DaireAlanı.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <cmath>
+float yaricapAl();
+void alanHesapla();
+float yaricap;
+float alan;
+const double PI = 3.14159265358979323846;
+
+
+int main()
+{
+    yaricapAl();
+    alanHesapla();
+}
+
+float yaricapAl()
+{
+    std::cout << "Lutfen dairenin yaricapini giriniz" << std::endl;
+    std::cin >> yaricap;
+    return yaricap;
+}
+
+void alanHesapla()
+{
+    alan = PI * pow(yaricap, 2);
+    std::cout << "Alan: " << alan << std::endl;
+}
+
