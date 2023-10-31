49e5da16b0a7c96c26e1ed3ade98fad18f6d2537
goktugycr <116160230+goktugycr@users.noreply.github.com>
2023-10-31 22:29:56 +0300

    Add files via upload

---
+++ BasamakAyırma.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <vector>
+
+int main() {
+    int sayi;
+    std::vector<int> basamaklar;
+
+    std::cout << "Lutfen bir sayi giriniz: ";
+    std::cin >> sayi;
+
+    while (sayi > 0) {
+        int basamak = sayi % 10;  
+        basamaklar.push_back(basamak); 
+        sayi = sayi / 10; 
+    }
+
+    std::cout << "Girilen sayinin basamaklari: ";
+    for (int i = basamaklar.size() - 1; i >= 0; i--) {
+        std::cout << basamaklar[i] << " ";
+    }
+
+    return 0;
+}
+
+
---
+++ GirilenSayiyaKadarToplama.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    int sayi;
+    int toplam = 0;
+
+    cout << "Lutfen bir sayi giriniz: ";
+    cin >> sayi;
+
+    
+    for (int i = 1; i <= sayi; i++) {
+        toplam += i;  
+    }
+
+    cout << "1'den " << sayi << "'e kadar olan sayilarin toplami: " << toplam << endl;
+
+    return 0;
+}
+
+
